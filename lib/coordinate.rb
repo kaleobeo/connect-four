72dcc7935cd986a914ebe19e4d39c3461089070e
Kaleo K <kaleo.kronberg@gmail.com>
2022-06-07 17:45:15 -0700

    Add the rest of the orthogonal methods to coordinate

--- lib/coordinate.rb
+++ lib/coordinate.rb
@@ -15,6 +15,21 @@ class Coordinate
     up_coord_height < @max_row ? Coordinate.new(col, up_coord_height) : NullCoordinate.new
   end
 
+  def down
+    down_coord_height = row - 1
+    down_coord_height.positive? ? Coordinate.new(col, down_coord_height) : NullCoordinate.new
+  end
+
+  def left
+    left_coord_col = col - 1
+    left_coord_col.between?(1, 7) ? Coordinate.new(left_coord_col, row) : NullCoordinate.new 
+  end
+
+  def right
+    right_coord_col = col + 1
+    right_coord_col.between?(1, 7) ? Coordinate.new(right_coord_col, row) : NullCoordinate.new
+  end
+
   def ==(other)
     return super unless other.is_a?(Coordinate)
 
--- spec/coordinate_spec.rb
+++ spec/coordinate_spec.rb
@@ -23,4 +23,67 @@ describe Coordinate do
       end
     end
   end
+
+  describe '#down' do
+    context 'when the position below is in bounds' do
+      subject(:valid_down) { described_class.new(4, 3) }
+
+      it 'returns the Coordinate below it' do
+        coord = valid_down.down
+        expected = described_class.new(4, 2)
+        expect(coord).to eq expected
+      end
+    end
+
+    context 'when the position below is not in bounds' do
+      subject(:invalid_down) { described_class.new(1, 1) }
+
+      it 'return a NullCoordinate' do
+        coord = invalid_down.down
+        expect(coord).to be_instance_of(NullCoordinate)
+      end
+    end
+  end
+
+  describe '#left' do
+    context 'when the position to the left is in bounds' do
+      subject(:valid_left) { described_class.new(4, 3) }
+
+      it 'returns the Coordinate left of it' do
+        coord = valid_left.left
+        expected = described_class.new(3, 3)
+        expect(coord).to eq expected
+      end
+    end
+
+    context 'when the position left is not in bounds' do
+      subject(:invalid_left) { described_class.new(1, 1) }
+
+      it 'return a NullCoordinate' do
+        coord = invalid_left.left
+        expect(coord).to be_instance_of(NullCoordinate)
+      end
+    end
+  end
+
+  describe '#right' do
+    context 'when the position to the right is in bounds' do
+      subject(:valid_right) { described_class.new(4, 3) }
+
+      it 'returns the Coordinate left of it' do
+        coord = valid_right.right
+        expected = described_class.new(5, 3)
+        expect(coord).to eq expected
+      end
+    end
+
+    context 'when the position to the right is not in bounds' do
+      subject(:invalid_right) { described_class.new(7, 1) }
+
+      it 'return a NullCoordinate' do
+        coord = invalid_right.right
+        expect(coord).to be_instance_of(NullCoordinate)
+      end
+    end
+  end
 end
